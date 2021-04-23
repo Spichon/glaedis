8f960197310d0fed55c40ec7cb2c62a5e0127f92
Simon Pichon <simon.a.pichon@octo.com>
2021-04-23 14:44:39 +0200

    modify travis

--- scripts/generate-env-front.sh
+++ scripts/generate-env-front.sh
@@ -7,4 +7,7 @@ COGNITO_USER_POOL_CLIENT_ID=$(aws cloudformation describe-stacks --stack-name gl
 COGNITO_USER_POOL_ID=$(aws cloudformation describe-stacks --stack-name glaedis-cognito-master --query "Stacks[0].Outputs[?OutputKey=='CognitoUserPoolId'].OutputValue" --output text)
 AWS_REGION=eu-west-1
 REDIRECT_URL=https://www.${ROOT_DOMAIN}
+ROOT_DOMAIN=${ROOT_DOMAIN}
+USER_API=https://user.${ROOT_DOMAIN}
+TEST_API=https://test.${ROOT_DOMAIN}
 EOL
