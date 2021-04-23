fa62b3fe10ddebaaaf17bb07cc51bf13afd64ce6
Simon Pichon <simon.a.pichon@octo.com>
2021-04-23 14:41:14 +0200

    modify travis

---
+++ scripts/generate-env-front.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+touch .env
+
+cat > .env << EOL
+COGNITO_USER_POOL_CLIENT_ID=$(aws cloudformation describe-stacks --stack-name glaedis-cognito-master --query "Stacks[0].Outputs[?OutputKey=='CognitoUserPoolClientId'].OutputValue" --output text)
+COGNITO_USER_POOL_ID=$(aws cloudformation describe-stacks --stack-name glaedis-cognito-master --query "Stacks[0].Outputs[?OutputKey=='CognitoUserPoolId'].OutputValue" --output text)
+AWS_REGION=eu-west-1
+REDIRECT_URL=https://www.${ROOT_DOMAIN}
+EOL
