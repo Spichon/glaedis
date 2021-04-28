40f5fa9d19cd73d07a156b28f27f1bc460b965c5
Simon Pichon <simon.a.pichon@octo.com>
2021-04-28 18:50:29 +0200

    modify travis

--- scripts/generate-env-back_local.sh
+++ scripts/generate-env-back_local.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-cat > frontend/.env.temp <<EOL
+cat > .env.temp <<EOL
 DB_URL=$(aws cloudformation describe-stacks --stack-name glaedis-rds-stack-${TRAVIS_BRANCH} --query "Stacks[0].Outputs[?OutputKey=='EndpointMaster'].OutputValue" --output text)
-DB_NAME=$(aws cloudformation describe-stacks --stack-name glaedis-rds-stack-${TRAVIS_BRANCH} --query "Stacks[0].Outputs[?OutputKey=='EndpointMaster'].OutputValue" --output text)
-VUE_APP_COGNITO_USER_POOL_ID=$(aws cloudformation describe-stacks --stack-name glaedis-cognito-${TRAVIS_BRANCH} --query "Stacks[0].Outputs[?OutputKey=='CognitoUserPoolId'].OutputValue" --output text)
-VUE_APP_AWS_REGION=eu-west-1
-VUE_APP_REDIRECT_URL=https://www.${ROOT_DOMAIN}/
-VUE_APP_ROOT_DOMAIN=${ROOT_DOMAIN}
-VUE_APP_USER_API=https://user.${ROOT_DOMAIN}
-VUE_APP_TEST_API=https://test.${ROOT_DOMAIN}
+DB_URL_REPLICA=$(aws cloudformation describe-stacks --stack-name glaedis-rds-stack-${TRAVIS_BRANCH} --query "Stacks[0].Outputs[?OutputKey=='PortReadReplica'].OutputValue" --output text)
+DB_NAME=$(aws cloudformation describe-stacks --stack-name glaedis-rds-stack-${TRAVIS_BRANCH} --query "Stacks[0].Outputs[?OutputKey=='DBName'].OutputValue" --output text)
+DB_USER=$(aws cloudformation describe-stacks --stack-name glaedis-rds-stack-${TRAVIS_BRANCH} --query "Stacks[0].Outputs[?OutputKey=='DBUser'].OutputValue" --output text)
+DB_PASSWORD=$(aws cloudformation describe-stacks --stack-name glaedis-rds-stack-${TRAVIS_BRANCH} --query "Stacks[0].Outputs[?OutputKey=='DBPassword'].OutputValue" --output text)
+PGADMIN_DEFAULT_EMAIL=pgadmin4@pgadmin.org
+PGADMIN_DEFAULT_PASSWORD=admin
+RABBITMQ_DEFAULT_USER=guest
+RABBITMQ_DEFAULT_PASS=guest
 EOL
